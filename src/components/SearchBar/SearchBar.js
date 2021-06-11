0442fc39a472a058ee959842ee17ffd4b4c69742
Melissa Mondot <31637708+melissa-mondot@users.noreply.github.com>
2021-06-11 10:58:16 -0700

    Add SearchBar. Renders sort options

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -15,7 +15,20 @@ class SearchBar extends React.Component {
     });
   }
   render() {
-    return <div></div>;
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>{this.renderSortByOptions(sortByOptions)}</ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a href="#">Let's Go</a>
+        </div>
+      </div>
+    );
   }
 }
 
