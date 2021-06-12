12a707297149c5d93c4cb0641dd5966ab01b9651
Melissa Mondot <31637708+melissa-mondot@users.noreply.github.com>
2021-06-11 23:47:40 -0700

    Move dummy data to top level, pass props and map listings

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,26 @@ import SearchBar from "../SearchBar/SearchBar";
 import BusinessList from "../BusinessList/BusinessList";
 import "./App.css";
 
+const business = {
+  imageSrc: "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
+  name: "MarginOtto Pizzeria",
+  address: "1010 Paddington Way",
+  city: "Flavortown",
+  state: "NY",
+  zipCode: "10101",
+  category: "Italian",
+  rating: 4.5,
+  reviewCount: 90,
+};
+
+const businesses = [business, business, business, business, business, business];
+
 function App() {
   return (
     <div className="App">
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={businesses} />
     </div>
   );
 }
