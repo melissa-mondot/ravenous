18b169df51c4766a35364f10ddb70ad60c34b21c
Melissa Mondot <31637708+melissa-mondot@users.noreply.github.com>
2021-06-11 23:19:33 -0700

    App rendering with dummy data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,13 @@
 import SearchBar from "../SearchBar/SearchBar";
+import BusinessList from "../BusinessList/BusinessList";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <h1>ravenous</h1>
       <SearchBar />
+      <BusinessList />
     </div>
   );
 }
